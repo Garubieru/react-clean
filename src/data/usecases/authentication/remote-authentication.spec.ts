d5b15f41a5ef1ebbc815b0732021538ccdc38d82
Garubieru <gabrielmorishita23@gmail.com>
2022-01-03 19:00:25 -0300

    refactor: changed remote auth to use faker lib

--- src/data/usecases/authentication/remote-authentication.spec.ts
+++ src/data/usecases/authentication/remote-authentication.spec.ts
@@ -1,12 +1,13 @@
 import { HttpPostClientSpy } from '../../test/mock-http-client';
 import { RemoteAuthentication } from './remote-authentication';
+import faker from 'faker';
 
 type SutTypes = {
   sut: RemoteAuthentication;
   httpPostClientSpy: HttpPostClientSpy;
 };
 
-const createSut = (url: string = 'test_url'): SutTypes => {
+const createSut = (url: string = faker.internet.url()): SutTypes => {
   const httpPostClientSpy = new HttpPostClientSpy();
   const sut = new RemoteAuthentication(url, httpPostClientSpy);
   return { sut, httpPostClientSpy };
@@ -14,7 +15,7 @@ const createSut = (url: string = 'test_url'): SutTypes => {
 
 describe('RemoteAuthentication', () => {
   it('Should call HttpPostClient with correct URL', async () => {
-    const url = 'any_url';
+    const url = faker.internet.url();
     const { sut, httpPostClientSpy } = createSut(url);
     const httpPostSpy = jest.spyOn(httpPostClientSpy, 'post');
     await sut.auth();
