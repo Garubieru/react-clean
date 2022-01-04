860f187b648b10bd9638c80b2d4c552bb65caf44
Garubieru <gabrielmorishita23@gmail.com>
2022-01-04 09:23:40 -0300

    test: ensure RemoteAuthentication throws InvalidCredentials on 401

--- src/data/test/mock-http-client.ts
+++ src/data/test/mock-http-client.ts
@@ -1,11 +1,19 @@
 import { HttpPostClient, HttpPostParams } from '@/data/protocols/http/http-post-client';
+import { HttpResponse, HttpStatusCode } from '@/data/protocols/http/http-response';
 
 export class HttpPostClientSpy implements HttpPostClient {
   public url: string | null = null;
-  public body: unknown | null = null;
-  async post(params: HttpPostParams): Promise<void> {
-    const { url, body } = params;
-    this.url = url;
-    this.body = body;
+  public body: any | null = null;
+
+  public response: HttpResponse<any> = {
+    statusCode: HttpStatusCode.noContent,
+    data: this.body,
+  };
+
+  async post<TResponse>(params: HttpPostParams): Promise<HttpResponse<TResponse>> {
+    this.url = params.url;
+    this.body = params.body;
+
+    return await Promise.resolve(this.response);
   }
 }
--- src/data/usecases/authentication/remote-authentication.spec.ts
+++ src/data/usecases/authentication/remote-authentication.spec.ts
@@ -2,6 +2,8 @@ import faker from 'faker';
 import { RemoteAuthentication } from './remote-authentication';
 import { mockAuthentication } from '@/domain/test/mock-authentication';
 import { HttpPostClientSpy } from '@/data/test/mock-http-client';
+import { InvalidCredentialError } from '@/domain/errors/invalid-credentials-error';
+import { HttpStatusCode } from '@/data/protocols/http/http-response';
 
 type SutTypes = {
   sut: RemoteAuthentication;
@@ -27,5 +29,15 @@ describe('RemoteAuthentication', () => {
     const mockAuthParams = mockAuthentication();
     await sut.auth(mockAuthParams);
     expect(httpPostClientSpy.body).toEqual(mockAuthParams);
+  });
+
+  it('Should throw InvalidCredentialsError when response returns 401 statusCode', async () => {
+    const { sut, httpPostClientSpy } = createSut();
+    httpPostClientSpy.response = {
+      statusCode: HttpStatusCode.unathorized,
+      data: '',
+    };
+    const promise = sut.auth(mockAuthentication());
+    await expect(promise).rejects.toThrow(new InvalidCredentialError());
   });
 });
