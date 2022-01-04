b498086aea5bad255e80b6fb7014cedbb401780a
Garubieru <gabrielmorishita23@gmail.com>
2022-01-04 08:42:25 -0300

    refactor: changed imports to use use relative paths

--- src/data/test/mock-http-client.ts
+++ src/data/test/mock-http-client.ts
@@ -1,4 +1,4 @@
-import { HttpPostClient, HttpPostParams } from 'data/protocols/http/http-post-client';
+import { HttpPostClient, HttpPostParams } from '@/data/protocols/http/http-post-client';
 
 export class HttpPostClientSpy implements HttpPostClient {
   public url: string | null = null;
