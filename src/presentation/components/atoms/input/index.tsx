f541500714d8742b1d97e848cf67ed4d4d70ef67
Garubieru <gabrielmorishita23@gmail.com>
2022-01-04 18:42:51 -0300

    feat: added input autofill block

--- src/presentation/components/atoms/input/index.tsx
+++ src/presentation/components/atoms/input/index.tsx
@@ -11,9 +11,13 @@ type InputProps = React.DetailedHTMLProps<
 };
 
 const Input: React.FC<InputProps> = ({ errorMsg, ...props }) => {
+  const handleFocus = (e: React.FocusEvent<HTMLInputElement, Element>): void => {
+    const currentInput = e.currentTarget;
+    currentInput.readOnly = false;
+  };
   return (
     <div className={Styles.inputWrapper}>
-      <input {...props} />
+      <input {...props} readOnly onFocus={handleFocus} />
       <span>
         <FontAwesomeIcon icon={faExclamationCircle} />
       </span>
