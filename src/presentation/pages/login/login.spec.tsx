39d8e05d3c98cba8650166ec968d6fdc3b212732
Garubieru <gabrielmorishita23@gmail.com>
2022-01-05 11:42:04 -0300

    test: ensure error warning not render

--- src/presentation/pages/login/login.spec.tsx
+++ src/presentation/pages/login/login.spec.tsx
@@ -2,8 +2,10 @@ import React from 'react';
 import { render } from '@testing-library/react';
 import Login from '.';
 
-describe('', () => {
-  it('', () => {
-    render(<Login />);
+describe('Login', () => {
+  it('Should error warning not be visible', () => {
+    const { getByTestId } = render(<Login />);
+    const error = getByTestId('error-msg');
+    expect(error.classList.contains('hidden')).toBeTruthy();
   });
 });
