1353435293ae6c5f9c63d2bf40ae43d5a03b446f
Garubieru <gabrielmorishita23@gmail.com>
2022-01-05 12:01:51 -0300

    test: ensure that button be disabled on start

--- src/presentation/pages/login/login.spec.tsx
+++ src/presentation/pages/login/login.spec.tsx
@@ -3,9 +3,11 @@ import { render } from '@testing-library/react';
 import Login from '.';
 
 describe('Login Component', () => {
-  it('Should error warning not be visible on start', () => {
+  it('Should start screen with initial state', () => {
     const { getByTestId } = render(<Login />);
     const error = getByTestId('error-msg');
+    const button = getByTestId('login-button') as HTMLButtonElement;
     expect(error.classList.contains('hidden')).toBeTruthy();
+    expect(button.disabled).toBeTruthy();
   });
 });
