00fada185973923e6ab366d90bddc780e0e2307f
Garubieru <gabrielmorishita23@gmail.com>
2022-01-06 11:08:40 -0300

    feat: ensure button is disabled when loading

--- src/presentation/components/atoms/button/index.tsx
+++ src/presentation/components/atoms/button/index.tsx
@@ -2,16 +2,17 @@ import React from 'react';
 import Spinner from '../spinner';
 import Styles from './styles.scss';
 
-type ButtonProps = React.DetailedHTMLProps<
-  React.ButtonHTMLAttributes<HTMLButtonElement>,
-  HTMLButtonElement
-> & {
+interface ButtonProps
+  extends React.DetailedHTMLProps<
+    React.ButtonHTMLAttributes<HTMLButtonElement>,
+    HTMLButtonElement
+  > {
   isLoading?: boolean;
-};
+}
 
-const Button: React.FC<ButtonProps> = ({ isLoading, ...props }) => {
+const Button: React.FC<ButtonProps> = ({ isLoading, disabled, ...props }) => {
   return (
-    <button className={Styles.button} {...props}>
+    <button className={Styles.button} disabled={disabled || isLoading} {...props}>
       {!isLoading && props.children}
       {isLoading && <Spinner />}
     </button>
