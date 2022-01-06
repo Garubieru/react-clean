41470f59b367249a55a840032230421360c751b6
Garubieru <gabrielmorishita23@gmail.com>
2022-01-06 15:07:20 -0300

    test: ensure validation do not throw error when value is valid

--- src/presentation/validation/required-field/required-field-validation.spec.ts
+++ src/presentation/validation/required-field/required-field-validation.spec.ts
@@ -1,3 +1,5 @@
+import faker from 'faker';
+
 import { RequiredFieldError } from '@/presentation/validation/errors';
 import { RequiredFieldValidation } from './required-field-validation';
 
@@ -6,5 +8,11 @@ describe('RequiredFieldValidation', () => {
     const sut = new RequiredFieldValidation('field');
     const result = sut.validate('');
     expect(result).toEqual(new RequiredFieldError());
+  });
+
+  it('Should not throw error when value is empty', () => {
+    const sut = new RequiredFieldValidation('field');
+    const result = sut.validate(faker.random.words());
+    expect(result).toBeFalsy();
   });
 });
