4a7a28a6598a643f3b16035534faad8e034269e7
Garubieru <gabrielmorishita23@gmail.com>
2022-01-06 15:02:57 -0300

    fix: typo

--- src/presentation/validation/required-field/required-field-validation.spec.ts
+++ src/presentation/validation/required-field/required-field-validation.spec.ts
@@ -2,7 +2,7 @@ import { RequiredFieldError } from '@/presentation/validation/errors';
 import { RequiredFieldValidation } from './required-field-validation';
 
 describe('RequiredFieldValidation', () => {
-  it('Should throw error with is empty', () => {
+  it('Should throw error when value is empty', () => {
     const sut = new RequiredFieldValidation('field');
     const result = sut.validate('');
     expect(result).toEqual(new RequiredFieldError());
