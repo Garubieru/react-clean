a5fab19597cbf96f3cb5fc29b28d79a61f4e1c18
Garubieru <gabrielmorishita23@gmail.com>
2022-01-07 12:22:15 -0300

    refactor: moved sut creation to a factory method

--- src/presentation/validation/validators/validation-composite/validation-composite.spec.ts
+++ src/presentation/validation/validators/validation-composite/validation-composite.spec.ts
@@ -3,21 +3,34 @@ import { FieldValidationSpy } from '@/presentation/validation/validators/test';
 import { InvalidFieldError } from '@/presentation/validation/errors';
 import { ValidationComposite } from './validation-composite';
 
+type SutTypes = {
+  sut: ValidationComposite;
+  fieldsValidationsSpy: FieldValidationSpy[];
+};
+
+const createSut = (field: string): SutTypes => {
+  const fieldValidationSpy1 = new FieldValidationSpy(field);
+  const fieldValidationSpy2 = new FieldValidationSpy(field);
+  const validations = [fieldValidationSpy1, fieldValidationSpy2];
+  const sut = new ValidationComposite([fieldValidationSpy1, fieldValidationSpy2]);
+  return { sut, fieldsValidationsSpy: validations };
+};
+
 describe('ValidationComposite', () => {
   it('Should return first error found if any validation fails', () => {
-    const fieldValidationSpy1 = new FieldValidationSpy('any_field');
-    const fieldValidationSpy2 = new FieldValidationSpy('any_field');
-    fieldValidationSpy1.error = new InvalidFieldError('any_error_message_1');
-    fieldValidationSpy2.error = new InvalidFieldError('any_error_message_2');
-    const sut = new ValidationComposite([fieldValidationSpy1, fieldValidationSpy2]);
-    const result = sut.validate('any_field', faker.random.word());
-    expect(result).toBe('any_error_message_1');
+    const field = faker.database.column();
+    const { sut, fieldsValidationsSpy } = createSut(field);
+    const error1 = faker.random.word();
+    fieldsValidationsSpy[0].error = new InvalidFieldError(error1);
+    fieldsValidationsSpy[1].error = new InvalidFieldError(faker.random.word());
+    const result = sut.validate(field, faker.random.word());
+    expect(result).toBe(error1);
   });
 
   it('Should not return error if any validation succeeds', () => {
-    const fieldValidationSpy = new FieldValidationSpy('any_field');
-    const sut = new ValidationComposite([fieldValidationSpy]);
-    const result = sut.validate('any_field', faker.random.word());
+    const field = faker.database.column();
+    const { sut } = createSut(field);
+    const result = sut.validate(field, faker.random.word());
     expect(result).toBeFalsy();
   });
 });
