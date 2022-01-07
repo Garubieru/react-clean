f19aab53fe74f96234de71d6ef8f984cece1a6af
Garubieru <gabrielmorishita23@gmail.com>
2022-01-07 10:17:33 -0300

    refactor: added faker random word

--- src/presentation/validation/validators/min-length/min-length-validation.spec.ts
+++ src/presentation/validation/validators/min-length/min-length-validation.spec.ts
@@ -1,5 +1,6 @@
 import { MinLengthValidation } from './min-length-validation';
 import { InvalidFieldError } from '../../errors';
+import faker from 'faker';
 
 const createSut = (length = 5): MinLengthValidation => {
   return new MinLengthValidation('length', length);
@@ -13,15 +14,15 @@ describe('MinLengthValidation', () => {
 
   it('Should return Error if value is invalid', () => {
     const sut = createSut();
-    const result = sut.validate('aaa');
+    const result = sut.validate(faker.random.alphaNumeric(3));
     expect(result).toEqual(
       new InvalidFieldError(`Value must have more than ${sut.length} characters`),
     );
   });
 
   it('Should not return Error if value is valid', () => {
-    const sut = createSut(3);
-    const result = sut.validate('aaa');
+    const sut = createSut();
+    const result = sut.validate(faker.random.alphaNumeric(10));
     expect(result).toBeFalsy();
   });
 });
