408ef5b3675c8e90ea5d4f5baa298805e0426b48
Garubieru <gabrielmorishita23@gmail.com>
2022-01-07 12:11:26 -0300

    test: ensure ValidationComposite returns the first error found

--- src/presentation/validation/validators/validation-composite/validation-composite.spec.ts
+++ src/presentation/validation/validators/validation-composite/validation-composite.spec.ts
@@ -4,18 +4,18 @@ import { InvalidFieldError } from '@/presentation/validation/errors';
 import { ValidationComposite } from './validation-composite';
 
 describe('ValidationComposite', () => {
-  it('Should return error if any validation fails', () => {
+  it('Should return first error found if any validation fails', () => {
     const fieldValidationSpy1 = new FieldValidationSpy('any_field');
     const fieldValidationSpy2 = new FieldValidationSpy('any_field');
+    fieldValidationSpy1.error = new InvalidFieldError('any_error_message_1');
+    fieldValidationSpy2.error = new InvalidFieldError('any_error_message_2');
     const sut = new ValidationComposite([fieldValidationSpy1, fieldValidationSpy2]);
-    fieldValidationSpy2.error = new InvalidFieldError('any_error_message');
     const result = sut.validate('any_field', faker.random.word());
-    expect(result).toBe('any_error_message');
+    expect(result).toBe('any_error_message_1');
   });
 
   it('Should not return error if any validation succeeds', () => {
     const fieldValidationSpy = new FieldValidationSpy('any_field');
-    jest.spyOn(fieldValidationSpy, 'validate').mockReturnValueOnce(null);
     const sut = new ValidationComposite([fieldValidationSpy]);
     const result = sut.validate('any_field', faker.random.word());
     expect(result).toBeFalsy();
