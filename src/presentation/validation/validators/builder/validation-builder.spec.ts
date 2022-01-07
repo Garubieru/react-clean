3bad84549e6097c96ad1598472757d2181b0d4fe
Garubieru <gabrielmorishita23@gmail.com>
2022-01-07 12:56:10 -0300

    test: ensure ValidationBuilder has min and isEmail healper

--- src/presentation/validation/validators/builder/validation-builder.spec.ts
+++ src/presentation/validation/validators/builder/validation-builder.spec.ts
@@ -1,10 +1,27 @@
+import { EmailValidator } from '@/infra/validators/email-validator/email-validator';
 import { RequiredFieldValidation } from '@/presentation/validation/validators';
+import { EmailValidation } from '../email/email-validation';
+import { MinLengthValidation } from '../min-length/min-length-validation';
 import { ValidationBuilder as sut } from './validation-builder';
 
 describe('ValidationBuilder', () => {
-  it('Should return RequiredFieldValidation', () => {
+  afterEach(() => jest.clearAllMocks());
+
+  it('Should have RequiredFieldValidation', () => {
     const fieldName = 'any_field';
     const validations = sut.field(fieldName).required().build();
     expect(validations).toEqual([new RequiredFieldValidation(fieldName)]);
+  });
+
+  it('Should have MinLengthValidation', () => {
+    const fieldName = 'any_field';
+    const validations = sut.field(fieldName).min(5).build();
+    expect(validations).toEqual([new MinLengthValidation(fieldName, 5)]);
+  });
+
+  it('Should return EmailValidation', () => {
+    const fieldName = 'any_field';
+    const validations = sut.field(fieldName).isEmail().build();
+    expect(validations).toEqual([new EmailValidation(fieldName, new EmailValidator())]);
   });
 });
