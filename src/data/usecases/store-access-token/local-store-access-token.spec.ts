139f7c93f492d283d17f4247a7c51c145d5cabbd
Garubieru <gabrielmorishita23@gmail.com>
2022-01-10 12:25:03 -0300

    refactor: moved sut to sut factory

--- src/data/usecases/store-access-token/local-store-access-token.spec.ts
+++ src/data/usecases/store-access-token/local-store-access-token.spec.ts
@@ -2,10 +2,23 @@ import faker from 'faker';
 import { LocalStoreAccessToken } from './local-store.access-token';
 import { SetStorageSpy } from '@/data/test';
 
+type SutTypes = {
+  setStorageSpy: SetStorageSpy;
+  sut: LocalStoreAccessToken;
+};
+
+const createSut = (): SutTypes => {
+  const setStorageSpy = new SetStorageSpy();
+  const sut = new LocalStoreAccessToken(setStorageSpy);
+  return {
+    setStorageSpy,
+    sut,
+  };
+};
+
 describe('LocalStoreAccessToken', () => {
   it('Should call setStorage with correct value', async () => {
-    const setStorageSpy = new SetStorageSpy();
-    const sut = new LocalStoreAccessToken(setStorageSpy);
+    const { setStorageSpy, sut } = createSut();
     const accessToken = faker.datatype.uuid();
     await sut.store(accessToken);
     expect(setStorageSpy.key).toBe('accessToken');
