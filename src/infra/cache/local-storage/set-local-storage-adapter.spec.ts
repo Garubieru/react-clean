69e0c0106e122cf5994a0a278679308d23d8e3e3
Garubieru <gabrielmorishita23@gmail.com>
2022-01-10 13:03:46 -0300

    refactor: moved sut creation to a factory method

--- src/infra/cache/local-storage/set-local-storage-adapter.spec.ts
+++ src/infra/cache/local-storage/set-local-storage-adapter.spec.ts
@@ -2,10 +2,12 @@ import faker from 'faker';
 import 'jest-localstorage-mock';
 import { SetLocalStorageAdapter } from './set-local-storage-adapter';
 
+const createSut = (): SetLocalStorageAdapter => new SetLocalStorageAdapter();
+
 describe('SetLocalStorageAdapter', () => {
   beforeEach(() => localStorage.clear());
   it('Should class localStorage.setItem with correct params', async () => {
-    const sut = new SetLocalStorageAdapter();
+    const sut = createSut();
     const key = faker.database.column();
     const value = faker.datatype.uuid();
     await sut.set(key, value);
@@ -13,7 +15,15 @@ describe('SetLocalStorageAdapter', () => {
   });
 
   it('Should set localStorage with correct value', async () => {
-    const sut = new SetLocalStorageAdapter();
+    const sut = createSut();
+    const key = faker.database.column();
+    const value = faker.datatype.uuid();
+    await sut.set(key, value);
+    expect(localStorage.getItem(key)).toBe(value);
+  });
+
+  it('Should set localStorage with correct value', async () => {
+    const sut = createSut();
     const key = faker.database.column();
     const value = faker.datatype.uuid();
     await sut.set(key, value);
