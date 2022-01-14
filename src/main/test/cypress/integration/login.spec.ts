3a15c5005529fbfed03584e5c0c61797763f48bb
Garubieru <gabrielmorishita23@gmail.com>
2022-01-14 16:01:40 -0300

    test: ensure login renders with initial state

--- src/main/test/cypress/integration/login.spec.ts
+++ src/main/test/cypress/integration/login.spec.ts
@@ -8,5 +8,14 @@ describe('Login', () => {
       .get('svg')
       .should('have.class', 'fa-exclamation-circle');
     cy.getByTestId('email-status').should('contain.text', 'Field is required');
+
+    cy.getByTestId('password-status')
+      .get('svg')
+      .should('have.class', 'fa-exclamation-circle');
+    cy.getByTestId('password-status').should('contain.text', 'Field is required');
+
+    cy.getByTestId('login-button').should('be.disabled');
+
+    cy.getByTestId('error-msg').should('have.css', 'display', 'none');
   });
 });
