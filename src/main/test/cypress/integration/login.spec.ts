9396b171446881358123dd2d819a9af895e6922c
Garubieru <gabrielmorishita23@gmail.com>
2022-01-14 16:23:46 -0300

    test: ensure input throws errors when is invalid

--- src/main/test/cypress/integration/login.spec.ts
+++ src/main/test/cypress/integration/login.spec.ts
@@ -1,14 +1,18 @@
+import faker from 'faker';
+
 describe('Login', () => {
   beforeEach(() => {
     cy.visit('login');
   });
 
   it('Should render page with initial state', () => {
+    cy.getByTestId('email').should('have.attr', 'readonly');
     cy.getByTestId('email-status')
       .get('svg')
       .should('have.class', 'fa-exclamation-circle');
     cy.getByTestId('email-status').should('contain.text', 'Field is required');
 
+    cy.getByTestId('password').should('have.attr', 'readonly');
     cy.getByTestId('password-status')
       .get('svg')
       .should('have.class', 'fa-exclamation-circle');
@@ -17,5 +21,16 @@ describe('Login', () => {
     cy.getByTestId('login-button').should('be.disabled');
 
     cy.getByTestId('error-msg').should('have.css', 'display', 'none');
+  });
+
+  it('Should show error if input is invalid', () => {
+    cy.getByTestId('email').focus().type(faker.random.word());
+    cy.getByTestId('email-status').should('contain.text', 'Invalid email');
+
+    cy.getByTestId('password').focus().type(faker.datatype.string(2));
+    cy.getByTestId('password-status').should(
+      'contain.text',
+      'Value must have more than 3 characters',
+    );
   });
 });
