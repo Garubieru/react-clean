bd8384fd91c0212b8658e7848b25a0541ba8e08f
Garubieru <gabrielmorishita23@gmail.com>
2022-01-15 11:00:52 -0300

    fix: ensure store accessToken throws error if accessToken is falsy

--- src/data/usecases/store-access-token/local-store.access-token.ts
+++ src/data/usecases/store-access-token/local-store.access-token.ts
@@ -1,9 +1,11 @@
 import { SetStorage } from '@/data/protocols/cache';
+import { UnexpectedError } from '@/domain/errors';
 import { StoreAccessToken } from '@/domain/usecases';
 
 export class LocalStoreAccessToken implements StoreAccessToken {
   constructor(private readonly setStorage: SetStorage) {}
   async store(accessToken: string): Promise<void> {
+    if (!accessToken) throw new UnexpectedError();
     return await this.setStorage.set('accessToken', accessToken);
   }
 }
