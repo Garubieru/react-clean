7ca67f4951c1dfbe8c407b562a24e4d42fefce47
Garubieru <gabrielmorishita23@gmail.com>
2022-01-19 10:55:10 -0300

    feat: ensure SurveyList renders with correct style

--- src/presentation/pages/survey-list/index.tsx
+++ src/presentation/pages/survey-list/index.tsx
@@ -27,10 +27,39 @@ const MainHeader: React.FC = () => {
 const SurveyList: React.FC = () => {
   return (
     <PageWrapper header={MainHeader}>
-      <h1>oi</h1>
-      <h1>oi</h1>
-      <h1>oi</h1>
-      <h1>oi</h1>
+      <div className={Styles.surveyListContainer}>
+        <h2>Surveys</h2>
+        <ul className={Styles.surveys}>
+          <li className={Styles.surveyItem}>
+            <time className={Styles.surveyTime}>
+              <span className={Styles.surveyDay}>20</span>
+              <span className={Styles.surveyMonth}>12</span>
+              <span className={Styles.surveyYear}>2020</span>
+            </time>
+            <h3>What is your favorite framework duly do dole memem?</h3>
+            <footer>See results</footer>
+          </li>
+          <li className={Styles.surveyItem}>
+            <time className={Styles.surveyTime}>
+              <span className={Styles.surveyDay}>20</span>
+              <span className={Styles.surveyMonth}>12</span>
+              <span className={Styles.surveyYear}>2020</span>
+            </time>
+            <h3>What is your favorite framework?</h3>
+            <footer>See results</footer>
+          </li>
+
+          <li className={Styles.surveyItem}>
+            <time className={Styles.surveyTime}>
+              <span className={Styles.surveyDay}>20</span>
+              <span className={Styles.surveyMonth}>12</span>
+              <span className={Styles.surveyYear}>2020</span>
+            </time>
+            <h3>What is your favorite framework?</h3>
+            <footer>See results</footer>
+          </li>
+        </ul>
+      </div>
     </PageWrapper>
   );
 };
