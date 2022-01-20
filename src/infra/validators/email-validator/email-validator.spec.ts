4dc6caa91fb875605f0c962595d81786706b2af1
Garubieru <gabrielmorishita23@gmail.com>
2022-01-20 11:49:39 -0300

    refactor: changed EmailValidator tests

--- src/infra/validators/email-validator/email-validator.spec.ts
+++ src/infra/validators/email-validator/email-validator.spec.ts
@@ -7,39 +7,32 @@ const createSut = (): EmailValidator => {
   return sut;
 };
 
-const validateSut = (sut: EmailValidator, email = faker.internet.email()): boolean => {
-  return sut.validate(email);
-};
-
 const isEmailSpy = jest.spyOn(isEmail, 'validate');
 
 describe('EmailValidator', () => {
-  afterEach(() => {
-    jest.clearAllMocks();
-  });
-
+  beforeEach(() => jest.clearAllMocks());
   it('Should call isEmail.validate with correct value', () => {
     const sut = createSut();
     const email = faker.internet.email();
-    validateSut(sut, email);
+    sut.validate(email);
     expect(isEmailSpy).toHaveBeenCalledWith(email);
   });
 
   it('Should return true when email is valid', () => {
     const sut = createSut();
-    validateSut(sut);
+    sut.validate(faker.internet.email());
     expect(isEmailSpy.mock.results[0].value).toBeTruthy();
   });
 
   it('Should return false when email is invalid', () => {
     const sut = createSut();
-    validateSut(sut, faker.random.words());
+    sut.validate(faker.random.words());
     expect(isEmailSpy.mock.results[0].value).toBeFalsy();
   });
 
   it('Should return false when email is empty', () => {
     const sut = createSut();
-    validateSut(sut, '');
+    sut.validate('');
     expect(isEmailSpy.mock.results[0].value).toBeFalsy();
   });
 });
