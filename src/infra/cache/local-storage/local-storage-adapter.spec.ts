80cdb9c50eb5cedd257ed21c729f2d7a5f0c3ac2
Garubieru <gabrielmorishita23@gmail.com>
2022-01-20 13:01:12 -0300

    test: ensure LocalStorageAdapter.get returns correct value

--- src/infra/cache/local-storage/local-storage-adapter.spec.ts
+++ src/infra/cache/local-storage/local-storage-adapter.spec.ts
@@ -6,19 +6,45 @@ const createSut = (): LocalStorageAdapter => new LocalStorageAdapter();
 
 describe('LocalStorageAdapter', () => {
   beforeEach(() => localStorage.clear());
-  it('Should class localStorage.setItem with correct params', async () => {
-    const sut = createSut();
-    const key = faker.database.column();
-    const value = faker.datatype.uuid();
-    sut.set(key, value);
-    expect(localStorage.setItem).toHaveBeenCalledWith(key, value);
+  describe('set', () => {
+    it('Should class localStorage.setItem with correct params', () => {
+      const sut = createSut();
+      const key = faker.database.column();
+      const value = faker.datatype.uuid();
+      sut.set(key, value);
+      expect(localStorage.setItem).toHaveBeenCalledWith(key, value);
+    });
+
+    it('Should set localStorage with correct value', () => {
+      const sut = createSut();
+      const key = faker.database.column();
+      const value = faker.datatype.uuid();
+      sut.set(key, value);
+      expect(localStorage.getItem(key)).toBe(value);
+    });
   });
 
-  it('Should set localStorage with correct value', async () => {
-    const sut = createSut();
-    const key = faker.database.column();
-    const value = faker.datatype.uuid();
-    sut.set(key, value);
-    expect(localStorage.getItem(key)).toBe(value);
+  describe('get', () => {
+    it('Should call localStorage.getItem with correct value', () => {
+      const sut = createSut();
+      const key = faker.database.column();
+      sut.get(key);
+      expect(localStorage.getItem).toHaveBeenCalledWith(key);
+    });
+
+    it('Should return correct value if localStorage item is set', () => {
+      const sut = createSut();
+      const key = faker.database.column();
+      const item = faker.random.objectElement();
+      localStorage.setItem(key, item);
+      const result = sut.get(key);
+      expect(result).toEqual(item);
+    });
+
+    it('Should not return value if localStorage item is not set', () => {
+      const sut = createSut();
+      const result = sut.get(faker.database.column());
+      expect(result).toBeFalsy();
+    });
   });
 });
