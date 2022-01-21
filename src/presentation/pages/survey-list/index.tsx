dff155d0f9a61b6dccdce174389f54001d535aad
Garubieru <gabrielmorishita23@gmail.com>
2022-01-21 18:29:58 -0300

    feat: ensure SurveyList renders correct items on load

--- src/presentation/pages/survey-list/index.tsx
+++ src/presentation/pages/survey-list/index.tsx
@@ -1,17 +1,21 @@
-import React, { useEffect } from 'react';
+import React, { useEffect, useState } from 'react';
 import { MainHeader, PageWrapper } from '@/presentation/components';
 import { SurveyItem } from './components';
 import Styles from './styles.scss';
 import { LoadSurveyList } from '@/domain/usecases';
+import { SurveyModel } from '@/domain/models';
 
 type SurveyListProps = {
   loadSurveyList: LoadSurveyList;
 };
 
 const SurveyList: React.FC<SurveyListProps> = ({ loadSurveyList }) => {
+  const [surveyItems, setSurveyItems] = useState<SurveyModel[]>([]);
+
   useEffect(() => {
     const listSurveys = async (): Promise<void> => {
-      await loadSurveyList.list();
+      const items = await loadSurveyList.list();
+      setSurveyItems(items);
     };
 
     listSurveys();
@@ -20,11 +24,22 @@ const SurveyList: React.FC<SurveyListProps> = ({ loadSurveyList }) => {
     <PageWrapper header={<MainHeader />}>
       <div className={Styles.surveyListContainer}>
         <h2>Surveys</h2>
-        <ul className={Styles.surveys} data-testid="surveysList">
-          <SurveyItem className={Styles.survey} loading />
-          <SurveyItem className={Styles.survey} loading />
-          <SurveyItem className={Styles.survey} loading />
-          <SurveyItem className={Styles.survey} loading />
+        <ul className={Styles.surveys} data-testid="surveys-list">
+          {surveyItems.length === 0 ? (
+            <>
+              <SurveyItem className={Styles.survey} loading />
+              <SurveyItem className={Styles.survey} loading />
+              <SurveyItem className={Styles.survey} loading />
+              <SurveyItem className={Styles.survey} loading />
+            </>
+          ) : (
+            <>
+              {surveyItems.map((surveyItem) => (
+                <SurveyItem key={surveyItem.id} surveyData={surveyItem} />
+              ))}
+              S
+            </>
+          )}
         </ul>
       </div>
     </PageWrapper>
