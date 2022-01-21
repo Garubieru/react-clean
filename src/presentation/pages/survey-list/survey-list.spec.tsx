03bd5f6d8f705dc597b024c508b4af08098102a3
Garubieru <gabrielmorishita23@gmail.com>
2022-01-21 15:45:23 -0300

    refactor: moved sut to factory method

--- src/presentation/pages/survey-list/survey-list.spec.tsx
+++ src/presentation/pages/survey-list/survey-list.spec.tsx
@@ -3,9 +3,13 @@ import React from 'react';
 import { render, screen } from '@testing-library/react';
 import SurveyList from '.';
 
+const createSut = (): void => {
+  render(<SurveyList />);
+};
+
 describe('SurveyList Component', () => {
   it('Should render with 4 empty items on start', () => {
-    render(<SurveyList />);
+    createSut();
     const surveyList = screen.getByTestId('surveysList');
     expect(surveyList.querySelectorAll('li:empty')).toHaveLength(4);
   });
