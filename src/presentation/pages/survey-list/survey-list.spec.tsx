d63f15a2ffa6eae39e30e72daa807f6f9d3cd4db
Garubieru <gabrielmorishita23@gmail.com>
2022-01-21 18:30:28 -0300

    test: ensure SurveyList renders correct items on load

--- src/presentation/pages/survey-list/survey-list.spec.tsx
+++ src/presentation/pages/survey-list/survey-list.spec.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { render, screen } from '@testing-library/react';
+import { render, screen, waitFor } from '@testing-library/react';
 
 import { LoadSurveyList } from '@/domain/usecases';
 import SurveyList from '.';
@@ -8,12 +8,13 @@ import { mockSurveyList } from '@/domain/test';
 
 class LoadSurveyListStub implements LoadSurveyList {
   public callsCount = 0;
+  public surveyList = mockSurveyList(this.surveyItemsLength);
 
   constructor(public surveyItemsLength: number) {}
 
   async list(): Promise<SurveyModel[]> {
     this.callsCount++;
-    return await Promise.resolve(mockSurveyList(this.surveyItemsLength));
+    return await Promise.resolve(this.surveyList);
   }
 }
 
@@ -30,14 +31,25 @@ const createSut = (surveyItemsLength: number = 5): SutTypes => {
 };
 
 describe('SurveyList Component', () => {
-  it('Should render with 4 empty items on start', () => {
+  it('Should render with 4 empty items on start', async () => {
     createSut();
-    const surveyList = screen.getByTestId('surveysList');
+    const surveyList = screen.getByTestId('surveys-list');
     expect(surveyList.querySelectorAll('li:empty')).toHaveLength(4);
+    await waitFor(() => surveyList);
   });
 
-  it('Should call LoadSurveyList only once when component is rendered', () => {
+  it('Should call LoadSurveyList only once when component is rendered', async () => {
     const { loadSurveyListStub } = createSut();
     expect(loadSurveyListStub.callsCount).toBe(1);
+    await waitFor(() => screen.getByRole('heading'));
+  });
+
+  it('Should render all SurveyItems properly', async () => {
+    const surveyListLength = 10;
+    createSut(surveyListLength);
+    const surveyList = screen.getByTestId('surveys-list');
+    expect(surveyList.querySelectorAll('li:empty')).toHaveLength(4);
+    await waitFor(() => surveyList);
+    expect(surveyList.querySelectorAll('li:not(:empty)')).toHaveLength(10);
   });
 });
