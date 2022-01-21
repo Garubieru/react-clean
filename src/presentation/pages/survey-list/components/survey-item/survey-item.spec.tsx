ccd081b8f4325dc60ba2971e1500b8c22e8399aa
Garubieru <gabrielmorishita23@gmail.com>
2022-01-21 18:17:37 -0300

    refactor: changed test order

--- src/presentation/pages/survey-list/components/survey-item/survey-item.spec.tsx
+++ src/presentation/pages/survey-list/components/survey-item/survey-item.spec.tsx
@@ -9,32 +9,14 @@ import SurveyItem from '.';
 
 const createSut = (
   loading: boolean = false,
-  didAnswer: boolean = false,
   surveyData: SurveyModel = mockSurvey(),
 ): HTMLElement => {
-  surveyData.didAnswer = didAnswer;
-  surveyData.date = new Date('2020-01-10T00:00:00');
   render(<SurveyItem loading={loading} surveyData={surveyData} />);
   return screen.getByTestId('survey-item');
 };
 
 const getSurveyElement = (elName: string): HTMLElement => {
   return screen.getByTestId('survey-item').querySelector(`.survey${elName}`);
-};
-
-const testSurveyElement = (surveyData: SurveyModel, didAnswer: boolean): void => {
-  createSut(false, didAnswer, surveyData);
-  expect(getSurveyElement('Icon')).toHaveClass(
-    surveyData.didAnswer ? 'success' : 'warning',
-  );
-  expect(screen.getByTestId('icon-image')).toHaveAttribute(
-    'src',
-    surveyData.didAnswer ? IconStatus.thumbsUp : IconStatus.thumbsDown,
-  );
-  expect(getSurveyElement('Day')).toHaveTextContent('10');
-  expect(getSurveyElement('Month')).toHaveTextContent('jan');
-  expect(getSurveyElement('Year')).toHaveTextContent('2020');
-  expect(getSurveyElement('Question')).toHaveTextContent(surveyData.question);
 };
 
 describe('SurveyItem Component', () => {
@@ -45,11 +27,30 @@ describe('SurveyItem Component', () => {
 
   it('Should render SurveyItem with correct surveyData if survey is answered', () => {
     const surveyData = mockSurvey();
-    testSurveyElement(surveyData, false);
+    surveyData.didAnswer = false;
+    surveyData.date = new Date('2020-01-10T00:00:00');
+    createSut(false, surveyData);
+    expect(getSurveyElement('Icon')).toHaveClass('warning');
+    expect(screen.getByTestId('icon-image')).toHaveAttribute(
+      'src',
+      IconStatus.thumbsDown,
+    );
+    expect(getSurveyElement('Day')).toHaveTextContent('10');
+    expect(getSurveyElement('Month')).toHaveTextContent('jan');
+    expect(getSurveyElement('Year')).toHaveTextContent('2020');
+    expect(getSurveyElement('Question')).toHaveTextContent(surveyData.question);
   });
 
   it('Should render SurveyItem with correct surveyData if survey is not answered', () => {
     const surveyData = mockSurvey();
-    testSurveyElement(surveyData, true);
+    surveyData.didAnswer = true;
+    surveyData.date = new Date('2019-03-03T00:00:00');
+    createSut(false, surveyData);
+    expect(getSurveyElement('Icon')).toHaveClass('success');
+    expect(screen.getByTestId('icon-image')).toHaveAttribute('src', IconStatus.thumbsUp);
+    expect(getSurveyElement('Day')).toHaveTextContent('03');
+    expect(getSurveyElement('Month')).toHaveTextContent('mar');
+    expect(getSurveyElement('Year')).toHaveTextContent('2019');
+    expect(getSurveyElement('Question')).toHaveTextContent(surveyData.question);
   });
 });
