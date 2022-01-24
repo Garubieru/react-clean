3aaff2a64a1cd72fa960a34c7df259e27571884f
Garubieru <gabrielmorishita23@gmail.com>
2022-01-24 10:51:27 -0300

    feat: ensures SurveyList renders error on failure

--- src/presentation/pages/survey-list/index.tsx
+++ src/presentation/pages/survey-list/index.tsx
@@ -10,37 +10,54 @@ type SurveyListProps = {
 };
 
 const SurveyList: React.FC<SurveyListProps> = ({ loadSurveyList }) => {
-  const [surveyItems, setSurveyItems] = useState<SurveyModel[]>([]);
+  const [surveyScreen, setSurveyScreen] = useState({
+    surveyItems: [] as SurveyModel[],
+    error: '',
+  });
 
   useEffect(() => {
     const listSurveys = async (): Promise<void> => {
-      const items = await loadSurveyList.list();
-      setSurveyItems(items);
+      try {
+        const items = await loadSurveyList.list();
+        setSurveyScreen((state) => ({ ...state, surveyItems: items }));
+      } catch (e) {
+        const error = e as Error;
+        setSurveyScreen((state) => ({ ...state, error: error.message }));
+      }
     };
 
     listSurveys();
-  });
+  }, []);
+
   return (
     <PageWrapper header={<MainHeader />}>
       <div className={Styles.surveyListContainer}>
         <h2>Surveys</h2>
-        <ul className={Styles.surveys} data-testid="surveys-list">
-          {surveyItems.length === 0 ? (
-            <>
-              <SurveyItem className={Styles.survey} loading />
-              <SurveyItem className={Styles.survey} loading />
-              <SurveyItem className={Styles.survey} loading />
-              <SurveyItem className={Styles.survey} loading />
-            </>
-          ) : (
-            <>
-              {surveyItems.map((surveyItem) => (
-                <SurveyItem key={surveyItem.id} surveyData={surveyItem} />
-              ))}
-              S
-            </>
-          )}
-        </ul>
+        {!surveyScreen.error && (
+          <ul className={Styles.surveys} data-testid="surveys-list">
+            {surveyScreen.surveyItems.length === 0 ? (
+              <>
+                <SurveyItem className={Styles.survey} loading />
+                <SurveyItem className={Styles.survey} loading />
+                <SurveyItem className={Styles.survey} loading />
+                <SurveyItem className={Styles.survey} loading />
+              </>
+            ) : (
+              <>
+                {surveyScreen.surveyItems.map((surveyItem) => (
+                  <SurveyItem key={surveyItem.id} surveyData={surveyItem} />
+                ))}
+                S
+              </>
+            )}
+          </ul>
+        )}
+        {surveyScreen.error && (
+          <div data-testid="error-wrap">
+            <span>{surveyScreen.error}</span>
+            <button>Reload</button>
+          </div>
+        )}
       </div>
     </PageWrapper>
   );
