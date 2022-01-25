49360c3296b308759ada3e5a9e87a31c582d8c2a
Garubieru <gabrielmorishita23@gmail.com>
2022-01-25 12:40:01 -0300

    feat: ensures MainHeader renders correct name and redirect on signOut

--- src/presentation/components/molecules/main-header/index.tsx
+++ src/presentation/components/molecules/main-header/index.tsx
@@ -3,19 +3,35 @@ import { faUserCircle, faSignOutAlt } from '@fortawesome/free-solid-svg-icons';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { Logo } from '@/presentation/components';
 import Styles from './styles.scss';
+import { useApi } from '@/presentation/context/api/api-context';
+import { useNavigate } from 'react-router-dom';
 
 const MainHeader: React.FC = () => {
-  // const { getLoginAccount } = useApi();
-  // const userName = getLoginAccount().name;
+  const { setLoginAccount, getLoginAccount } = useApi();
+  const { name } = getLoginAccount();
+  const navigate = useNavigate();
+  const handleSignout = (e: React.MouseEvent<HTMLLIElement, MouseEvent>): void => {
+    e.preventDefault();
+    e.stopPropagation();
+    setLoginAccount(null);
+    navigate('/login');
+  };
+
   return (
     <header className={Styles.mainHeader}>
       <Logo className={Styles.navLogo} />
       <div className={Styles.userContainerHeader}>
-        <p className={Styles.userName}>q</p>
+        <p className={Styles.userName} data-testid="user-name">
+          {name}
+        </p>
         <FontAwesomeIcon className={Styles.userIcon} icon={faUserCircle} />
         <div className={Styles.userOptionsContainer}>
           <ul className={Styles.userOptionsList}>
-            <li className={Styles.userOptionsItem}>
+            <li
+              data-testid="signout-link"
+              className={Styles.userOptionsItem}
+              onClick={handleSignout}
+            >
               <a href="/login">Signout</a>
               <FontAwesomeIcon className={Styles.optionIcon} icon={faSignOutAlt} />
             </li>
