15d2126d640205f877689a17f12c6ccccce8a1e7
Garubieru <gabrielmorishita23@gmail.com>
2022-01-28 12:59:43 -0300

    refactor: changed SurveyList load test

--- src/main/test/cypress/integration/survey-list.spec.ts
+++ src/main/test/cypress/integration/survey-list.spec.ts
@@ -1,28 +1,13 @@
-import faker from 'faker';
 import * as HttpMocks from '../utils/http-mocks';
 import * as SurveyHelpers from '../utils/survey-helpers';
 import * as Helpers from '../utils/helpers';
 
-type SurveyItem = {
-  id: string;
-  date: Date;
-  question: string;
-  didAnswer: boolean;
-};
-
-export const mockSurveyList = (length: number = 2): SurveyItem[] => {
-  const mockSurveyItem = (): SurveyItem => ({
-    id: faker.datatype.uuid(),
-    date: faker.date.recent(),
-    question: faker.random.words(),
-    didAnswer: faker.datatype.boolean(),
-  });
-  return [mockSurveyItem(), mockSurveyItem()];
-};
-
 const url = /surveys/;
-export const mockHttpSurveyListSuccess = (surveyList = mockSurveyList()): void => {
-  HttpMocks.mockSuccess('GET', url, surveyList);
+
+export const mockHttpSurveyListSuccess = (): void => {
+  cy.fixture('survey-list').then((surveyList) => {
+    HttpMocks.mockSuccess('GET', url, surveyList);
+  });
 };
 
 export const mockHttpSurveyForbidden = (): void => {
@@ -50,28 +35,25 @@ describe('SurveyList', () => {
     cy.getByTestId('error-wrap').should('have.text', 'An unexpected error ocurred.');
   });
 
-  it('Should show reload button on UnexpectedError and call api if is clicked', () => {
+  it('Should show reload button on UnexpectedError and recall api if is clicked', () => {
     mockHttpSurveyUnexpectedError();
     cy.visit('');
+    cy.wait('@request');
+    mockHttpSurveyListSuccess();
     cy.getByTestId('reload-button').click();
+    cy.getByTestId('surveys-list').children('li:not(:empty)').should('have.length', 2);
     Helpers.testApiCalls('request', 2);
   });
 
   it('Should render correct surveys on load', () => {
-    const mockedSurvey = mockSurveyList();
-    mockedSurvey[0].date = new Date('01-10-2020');
-    mockedSurvey[1].date = new Date('10-02-2020');
-    mockHttpSurveyListSuccess(mockedSurvey);
+    mockHttpSurveyListSuccess();
     cy.visit('');
     cy.getByTestId('surveys-list').children('li:empty').should('have.length', 4);
     cy.wait('@request');
-    cy.getByTestId('surveys-list')
-      .children('li:not(:empty)')
-      .should('have.length', mockedSurvey.length);
+    cy.getByTestId('surveys-list').children('li:not(:empty)').should('have.length', 2);
 
-    const [survey1, survey2] = mockedSurvey;
-    SurveyHelpers.testSurvey(0, survey1.question, '10', 'jan', '2020', survey1.didAnswer);
-    SurveyHelpers.testSurvey(1, survey2.question, '02', 'out', '2020', survey2.didAnswer);
+    SurveyHelpers.testSurvey(0, 'Question 1', '03', 'fev', '2018', true);
+    SurveyHelpers.testSurvey(1, 'Question 2', '13', 'out', '2020', false);
 
     Helpers.testApiCalls('request', 1);
   });
