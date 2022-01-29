40fd4c1277dc06ba577bf62b6f57481314391146
Garubieru <gabrielmorishita23@gmail.com>
2022-01-28 21:13:10 -0300

    chore: removed helmet

--- index.js
+++ index.js
@@ -1,10 +1,9 @@
 const fallback = require('express-history-api-fallback');
-const helmet = require('helmet');
 const express = require('express');
 const path = require('path');
 const app = express();
 const root = path.join(__dirname, '/dist');
 app.use(express.static(root));
 app.use(fallback('index.html', { root }));
-app.use(helmet());
+
 app.listen(process.env.PORT || 3000);
