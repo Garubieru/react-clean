c985c25e12e06c192228506a3a13e6d955c5fbbf
Garubieru <gabrielmorishita23@gmail.com>
2022-02-04 16:44:04 -0300

    feat: add RemoteSurveyList response on 200

--- src/data/usecases/load-survey-result/load-survey-result.ts
+++ src/data/usecases/load-survey-result/load-survey-result.ts
@@ -1,18 +1,33 @@
 import { HttpGetClient, HttpStatusCode } from '@/data/protocols/http';
 import { ForbiddenError, UnexpectedError } from '@/domain/errors';
+import { LoadSurveyResult } from '@/domain/usecases';
+
+export namespace RemoteLoadSurveyResult {
+  export type Model = {
+    question: string;
+    answers: Array<{
+      image: string;
+      answer: string;
+      count: number;
+      percent: number;
+      isCurrentAccountAnswer: boolean;
+    }>;
+    date: string;
+  };
+}
 
 export class RemoteLoadSurveyResult {
   constructor(
     private readonly url: string,
-    private readonly httpGetClient: HttpGetClient,
+    private readonly httpGetClient: HttpGetClient<RemoteLoadSurveyResult.Model>,
   ) {}
 
-  async load(): Promise<void> {
-    const { statusCode } = await this.httpGetClient.get({ url: this.url });
+  async load(): Promise<LoadSurveyResult.Model> {
+    const { statusCode, body } = await this.httpGetClient.get({ url: this.url });
 
     switch (statusCode) {
       case HttpStatusCode.ok: {
-        return;
+        return Object.assign(body, { date: new Date(body.date) });
       }
       case HttpStatusCode.forbidden: {
         throw new ForbiddenError();
