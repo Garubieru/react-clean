ddf6de705a9fb8fedf21df90cbe4fe6da8758a8e
Garubieru <gabrielmorishita23@gmail.com>
2022-02-04 15:04:17 -0300

    feat: ensure LoadSurveyResult throws Forbidden

--- src/data/usecases/load-survey-result/load-survey-result.spec.ts
+++ src/data/usecases/load-survey-result/load-survey-result.spec.ts
@@ -1,5 +1,7 @@
+import { HttpStatusCode } from '@/data/protocols/http';
 import { HttpGetClientSpy } from '@/data/test';
 import { RemoteLoadSurveyResult } from '@/data/usecases';
+import { ForbiddenError } from '@/domain/errors';
 import faker from 'faker';
 
 describe('RemoteLoadSurveyResult', () => {
@@ -9,5 +11,15 @@ describe('RemoteLoadSurveyResult', () => {
     const sut = new RemoteLoadSurveyResult(url, httpGetClient);
     sut.load();
     expect(httpGetClient.url).toBe(url);
+  });
+
+  it('Should throw ForbiddenError on 403', async () => {
+    const httpGetClient = new HttpGetClientSpy();
+    httpGetClient.response = {
+      statusCode: HttpStatusCode.forbidden,
+    };
+    const sut = new RemoteLoadSurveyResult(faker.internet.url(), httpGetClient);
+    const promise = sut.load();
+    await expect(promise).rejects.toBeInstanceOf(ForbiddenError);
   });
 });
